02d4410d4b27867c1938392b1e7b947bb72aaef3
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-07-28 19:56:16 -0300

    Custom Hook em uso, e projeto 'Almost Completed', só falta adicionar a funcionalidade do botão.

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,18 +1,27 @@
 import styles from '../styles/MainSection.module.css';
 import DivisorSVG from '../assets/pattern-divider-desktop.svg';
 import DiceSVG from '../assets/icon-dice.svg';
+import useFetch from '../customHooks/useFetch';
 
 const MainSection = () => {
+  const { isLoading, value, error } = useFetch("https://api.adviceslip.com/advice");
+
+  if (isLoading) {
+    return "Carregando...";
+  } else if (error) {
+    console.log(error);
+  };
+
   return (
-    <section>
+    <section className={styles.boxContent}>
       <p className={styles.adviceTitle}>
-        {}
+        Advice
         <span className={styles.adviceId}>
-          { }
+          #{value.slip.id}
         </span>
       </p>
       <h1 className={styles.adviceParagraph}>
-        { }
+        "{value.slip.advice}"
       </h1>
       <img src={DivisorSVG} alt="DivisorSVG" className={styles.dividerImg} />
       <span className={styles.diceSpan}>
