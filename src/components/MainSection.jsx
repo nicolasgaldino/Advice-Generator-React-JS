94435332f6056fb9a864212c3c71228b4f4c32fa
nicolasgaldino <nicolasesmael1998@gmail.com>
2022-07-31 13:16:57 -0300

    Depois de muito... Mutio... Mais muito trabalho, consegui aplicar o dark e light mode na aplicação.

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,35 +1,82 @@
-import styles from '../styles/MainSection.module.css';
+import React from 'react';
 import DivisorSVG from '../assets/pattern-divider-desktop.svg';
 import DiceSVG from '../assets/icon-dice.svg';
 import useFetch from '../customHooks/useFetch';
 import Spinner from './Spinner';
+import styles from "../styles/MainSection.module.css";
+const {
+  boxContent,
+  adviceTitle,
+  adviceId,
+  adviceParagraph,
+  dividerImg,
+  diceSpan,
+  diceBtn,
+  boxContentLight,
+  adviceTitleLight,
+  diceSpanLight,
+  buttonDIV,
+  changeButton,
+  changeButtonLight,
+} = styles;
 
 const MainSection = () => {
   const { isLoading, value, error, reFetch } = useFetch("https://api.adviceslip.com/advice");
+  const [theme, setTheme] = React.useState("dark");
+
+  React.useEffect(() => {
+
+    const localTheme = localStorage.getItem("theme");
+
+    if (localTheme !== undefined) {
+      setTheme(localTheme);
+
+      if (localTheme === "dark") {
+        document.getElementsByTagName("body")[0].classList.add("dark");
+      } else {
+        document.getElementsByTagName("body")[0].classList.remove("dark");
+      }
+    }
+  }, []);
+
+  const changeTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+      document.getElementsByTagName("body")[0].classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      setTheme("dark");
+      document.getElementsByTagName("body")[0].classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+  };
 
   if (isLoading) {
-    return <section className={styles.boxContent}> <Spinner /> </section>
+    return <section className={theme === "dark" ? boxContent : boxContentLight}> <Spinner /> </section>
   } else if (error) {
     console.log(error);
   }
 
   return (
-    <section className={styles.boxContent}>
-      <p className={styles.adviceTitle}>
-        Advice
-        <span className={styles.adviceId}>
-          #{value.slip.id}
-        </span>
-      </p>
-      <h1 className={styles.adviceParagraph}>
+    <section className={theme === "dark" ? boxContent : boxContentLight}>
+      <div className={buttonDIV}>
+        <p className={theme === "dark" ? adviceTitle : adviceTitleLight}>
+          Advice
+          <span className={adviceId}>
+            #{value.slip.id}
+          </span>
+        </p>
+        <button onClick={() => changeTheme()} className={theme === "dark" ? changeButton : changeButtonLight}>Trocar</button>
+      </div>
+      <h1 className={adviceParagraph}>
         "{value.slip.advice}"
       </h1>
-      <img src={DivisorSVG} alt="DivisorSVG" className={styles.dividerImg} />
-      <span className={styles.diceSpan}>
+      <img src={DivisorSVG} alt="DivisorSVG" className={dividerImg} />
+      <span className={theme === "dark" ? diceSpan : diceSpanLight}>
         <img
           src={DiceSVG}
           alt="DiceSVG"
-          className={styles.diceBtn}
+          className={diceBtn}
           onClick={() => reFetch()}
         />
       </span>
